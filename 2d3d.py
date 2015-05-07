d5b8e5fe914aa802b438488f8df93ae1e461795d
Ben Oliver <benjamin.e.oliver@gmail.com>
2015-05-07 11:51:48 +0200

    a simple working version

---
+++ 2d3d.py
@@ -0,0 +1,126 @@
+#! /usr/bin/env python3
+
+from random import shuffle
+from ntpath import basename
+from sys import argv
+from PIL import Image
+from collections import defaultdict, namedtuple
+from itertools import islice, cycle
+
+Point  = namedtuple('Point', ['point','colorID'])
+Points = namedtuple('Points', ['pointX', 'pointY', 'pointZ','colorX', ])
+
+
+def stringVertices(x,y,z):
+    """returns a string representation of min required vertices"""
+    x1 = x+1
+    y1 = y+1
+    z1 = z+1
+    vertices = [(x1,y,z),(x1,y1,z),(x1,y,z1),(x1,y1,z1),(x,y,z1),(x,y1,z1)]
+    return '\n'.join(map(lambda v: ' '.join(map(str,('v',) + v)), vertices))+'\n'
+
+
+def stringFaces(n, colorXstring, colorZstring):
+    """where n is a multiple of 6"""
+    x  = ('f',n+2,n+4,n+5,n+3)
+    z  = ('f',n,n+2,n+3,n+1)
+    return '\n'.join(["usemtl " + colorXstring,
+                      ' '.join(map(str,x)),
+                      "usemtl " + colorZstring,
+                      ' '.join(map(str,z))])+'\n'
+
+
+def mtlStringRGB(rgbVal,colorString):
+    kd = "Kd " + ' '.join(map(lambda x: str(x/255), list(rgbVal)))
+    xs = ["newmtl " + colorString,
+          "Ns 100",
+          kd,
+          "Ks 1 1 1",
+          "Ka 0 0 0",
+          "Ni 1","d 1",
+          "illum 1"]
+    return '\n'.join(xs) + '\n\n'
+
+
+# returns a dict that has y vals as keys with vals of lists [ Point(point=Int,colorID=String) ]
+def readPoints(image, colorMapPointer, ignore=(255,255,255)):
+    mode     = image.mode
+    (mX,mY)  = image.size
+    pix      = image.load()
+    d        = defaultdict(list)
+    stringID = str(id(image))
+    for y in range(mY):
+        for x in range(mX):
+            color = pix[x,y]
+            if color != ignore:
+                if color not in colorMapPointer:
+                    colorMapPointer[color] = stringID + str(y) + str(x)
+                d[mY-1-y].append(Point(x, colorMapPointer[color]))
+    return d
+
+
+# returns a dict that has (x,y,z) vals as keys with (colorIDx,colorIDy) as vals
+def mergePoints(pointsX, pointsZ):
+    mergedPoints = {}
+    for (y,xPoints) in pointsX.items():
+        if y in pointsZ:
+            zPoints = pointsZ.get(y)
+            mx = max(len(xPoints),len(zPoints))
+            for (x,z) in islice(zip(cycle(xPoints), cycle(zPoints)), mx):
+                mergedPoints[(x.point, y, z.point)] = (x.colorID, z.colorID)
+    return mergedPoints
+
+
+
+def mergePointsRandom(pointsX, pointsZ):
+    mergedPoints = {}
+    for (y,xPoints) in pointsX.items():
+        if y in pointsZ:
+            zPoints = pointsZ.get(y)
+            mx = max(len(xPoints),len(zPoints))
+            xs = list(islice(cycle(xPoints),mx))
+            ys = list(islice(cycle(zPoints),mx))
+            shuffle(xs)
+            shuffle(ys)
+            for (x,z) in zip(xs,ys):
+                mergedPoints[(x.point, y, z.point)] = (x.colorID, z.colorID)
+    return mergedPoints
+
+
+# mtlWriter only needs the COLORDICT
+def mtlWriter(filepath, colorDict):
+    with open(filepath+".mtl",'w') as openfile:
+        for (rgb,code) in colorDict.items():
+            openfile.write(mtlStringRGB(rgb,code))
+
+
+def objWriter(filepath, mergedPoints):
+    index = 1
+    with open(filepath+".obj", 'w') as openfile:
+        openfile.write("mtllib " + basename(filepath) + ".mtl\n")
+        for (x,y,z) in mergedPoints.keys():
+            openfile.write(stringVertices(x,y,z))
+        for (colorIDx, colorIDz) in mergedPoints.values():
+            openfile.write(stringFaces(index, colorIDx, colorIDz))
+            index += 6
+
+
+def run(fileA,fileB,outpath):
+    colorDict = {}
+    imgA = Image.open(fileA).convert('RGB')
+    imgB = Image.open(fileB).convert('RGB')
+    d1 = readPoints(imgA, colorDict)
+    d2 = readPoints(imgB, colorDict)
+    merged = mergePointsRandom(d1,d2)
+    # write mtl file
+    mtlWriter(outpath, colorDict)
+    objWriter(outpath, merged)
+
+
+if __name__ == '__main__':
+
+    a = argv[1]
+    b = argv[2]
+    out = argv[3]
+
+    run(a,b,out)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 2d3d
 2D or not 2D, that is the question...
+
+
+Example:
+
+cd ../2d3d/
+./2d3d.py ../2d3d/input/in_a.gif ../2d3d/input/in_b.gif ~/git/2d3d/output/out_ab
+
+infileA infileB outFile
+
+will create both .obj and .mtl files 
